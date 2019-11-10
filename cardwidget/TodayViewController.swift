13a8a502da7e01e1f4c97415822eb4d86601d019
Wexpo Lyu <laosb@lasteam.net>
2019-11-10 17:21:45 +0800

    feat(widgets/card): Finalizing.

---
+++ cardwidget/TodayViewController.swift
@@ -0,0 +1,78 @@
+//
+//  TodayViewController.swift
+//  sunrunwidget
+//
+//  Created by Shibo Lyu on 2019/11/8.
+//  Copyright © 2019 Inkwire Tech (Hangzhou) Co., Ltd. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import SwiftUI
+import Alamofire
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    func renderData(cardData: LMWidgetCard) {
+        let srVc = UIHostingController(rootView: CardWidget(cardData: cardData))
+        srVc.view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.0)
+        srVc.view.frame = self.view.bounds
+        self.view.addSubview(srVc.view)
+        self.addChild(srVc)
+        srVc.didMove(toParent: self)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sharedUd = UserDefaults.init(suiteName: "group.help.hdu.lemon.ios")
+        let token = sharedUd?.string(forKey: "token")
+        var cardData = LMWidgetCard()
+        if token != nil {
+            Alamofire.request("https://api.hduhelp.com/infoStream/v3", headers:["Authorization": "token \(token ?? "")"]).validate().responseJSON(completionHandler:
+                {
+                    response in switch response.result
+                    {
+                        case .success:
+//                            print(response.result)
+                            let json = response.result.value
+                            let newRawData = (json as! NSDictionary).object(forKey: "data") as! NSDictionary
+                            let CardData = newRawData.object(forKey: "card") as? NSDictionary
+                            
+                            cardData.available = CardData?.object(forKey: "available") as? Bool
+                            let indexData = CardData?.object(forKey: "data") as? NSDictionary
+                            
+                            let remaining = indexData?.object(forKey: "remaining") as? Double ?? 0
+                            cardData.remaining = String(format:"%.2lf", remaining)
+                            let today = indexData?.object(forKey: "today") as? Double ?? 0
+                            cardData.today = String(format:"%.2lf", today)
+                            
+//                            if remaining <= 15.0 {
+                                cardData.tip = "⚠️ 余额较少，记得充值"
+//                            }
+                            
+                            self.renderData(cardData: cardData)
+                        case .failure:
+//                            print(response)
+                            cardData.available = false
+                            self.renderData(cardData: cardData)
+                    }
+                }
+            )
+        } else {
+            cardData.available = false
+            self.renderData(cardData: cardData)
+        }
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
