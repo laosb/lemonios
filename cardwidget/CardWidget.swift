cde4328b08e7ae60152b5d3e97937a0de9461675
Wexpo Lyu <laosb@lasteam.net>
2019-11-20 17:36:42 +0800

    feat(widget/card): Add charge function.

--- cardwidget/CardWidget.swift
+++ cardwidget/CardWidget.swift
@@ -18,28 +18,51 @@ struct LMWidgetCard {
 //@available(iOS 13.0.0, *)
 struct CardWidget: View {
     
+    @State var tipMessage: String = ""
+    
     var cardData: LMWidgetCard?
+    var chargeFunc: (() -> Bool)?
     var body: some View {
         
         VStack (alignment: .center){
             if cardData?.available ?? false {
-                VStack {
-                    HStack(alignment: .center) {
-                        Spacer()
-                        WidgetNumberView(number: self.cardData?.remaining ?? "", title: "余额", unit: "元")
-                        Spacer()
-                        WidgetNumberView(number: self.cardData?.today ?? "", title: "今日消费", unit: "元")
-                        Spacer()
-                    }
-                    Text(self.cardData?.tip ?? "")
-                        .font(.footnote)
-                        .foregroundColor(.secondary)
-                }.padding()
+                HStack {
+                    VStack {
+                        HStack(alignment: .center) {
+                            Spacer()
+                            WidgetNumberView(number: self.cardData?.remaining ?? "", title: "余额", unit: "元")
+                            Spacer()
+                            WidgetNumberView(number: self.cardData?.today ?? "", title: "今日消费", unit: "元")
+                            Spacer()
+                        }
+                        Text(tipMessage != "" ? tipMessage : self.cardData?.tip ?? "")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }.padding()
+                    HStack {
+                        Button(action: {
+                            let result = self.chargeFunc?()
+                            if !(result ?? false) {
+                                self.tipMessage = "⚠️充值依赖支付宝。请确认您安装了支付宝。"
+                            }
+                        }) {
+                            VStack {
+                                Spacer()
+                                Text("充值")
+                                    .accentColor(Color.white)
+                                Spacer()
+                            }
+                        }
+                            .frame(width: 25.0)
+                            .background(Color(red:0.20, green:0.60, blue:0.86))
+                            .cornerRadius(12.5)
+                    }.padding(10.0)
+                }
             } else {
                 Text("数据获取失败。请尝试打开杭电助手并登录。")
                     .foregroundColor(.secondary)
             }
-        }.padding()
+        }
     }
 }
 
