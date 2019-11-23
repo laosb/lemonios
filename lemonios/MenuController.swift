3a281cc01bd0ac31f3153649d59749f2014742d3
Wexpo Lyu <laosb@lasteam.net>
2019-11-23 16:34:34 +0800

    fix(catalyst): remove extra menus.

---
+++ lemonios/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  lemonios
+//
+//  Created by Shibo Lyu on 2019/11/23.
+//  Copyright © 2019 Inkwire Tech (Hangzhou) Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MenuController {
+    init(with builder: UIMenuBuilder) {
+        builder.remove(menu: .format)
+        builder.remove(menu: .file)
+    }
+}
