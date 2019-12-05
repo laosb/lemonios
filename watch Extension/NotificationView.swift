cf485428bddc63ccbc9d442121c818f4145076d9
Wexpo Lyu <laosb@lasteam.net>
2019-12-06 02:41:30 +0800

    feat(watch): Init.

---
+++ watch Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  watch Extension
+//
+//  Created by Shibo Lyu on 2019/12/6.
+//  Copyright © 2019 Inkwire Tech (Hangzhou) Co., Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
