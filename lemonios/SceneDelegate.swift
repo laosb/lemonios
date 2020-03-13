b11c712193825d5dd5ea10d5c52181e95fd2d526
Shibo Lyu <laosb@lns.unihubs.com>
2020-03-14 01:25:28 +0800

    fix: universal links & shortcut.

--- lemonios/SceneDelegate.swift
+++ lemonios/SceneDelegate.swift
@@ -14,28 +14,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    let app = UIApplication.shared.delegate as! AppDelegate
+    
     func routeUrl(urlContexts: Set<UIOpenURLContext>) {
         if let url = urlContexts.first?.url {
-            guard
-                let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true)
-            else { return }
-            
-            let path = components.path
-            let params = components.queryItems
-            let hash = components.fragment
-            
-            let app = UIApplication.shared.delegate as! AppDelegate
-            let naviVc = window?.rootViewController as? UINavigationController
-            let vc = naviVc?.topViewController as? ViewController
-
-            if path == "/login", let auth = params?.first(where: { $0.name == "auth" }) {
-                app.token = auth.value
-                vc?.shortcutFired(nativeLogin: false, route: nil)
-            } else {
-                vc?.shortcutFired(nativeLogin: false, route: hash)
-            }
+            app.routeUrl(url: url, window: window)
         }
     }
+    
+    func routeUrl(url: URL) {
+        app.routeUrl(url: url, window: window)
+    }
+    
+    func routeShortcut(_ shortcut: String) {
+        let activities = ["schedule", "card"]
+        let sc = String(shortcut.split(separator: ".").last ?? "")
+        if activities.contains(sc) {
+            app.shortcutName = sc
+            
+            let naviVc = window?.rootViewController as? UINavigationController
+            let vc = naviVc?.topViewController as? ViewController
+            vc?.shortcutFired(nativeLogin: false, route: nil)
+        }
+    }
+
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the
@@ -57,7 +59,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
         #endif
         print("url in first")
+        if let shortcutItem = connectionOptions.shortcutItem {
+            routeShortcut(shortcutItem.type)
+        }
         routeUrl(urlContexts: connectionOptions.urlContexts)
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        print("activity", userActivity.activityType)
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+            if let url = userActivity.webpageURL {
+                routeUrl(url: url)
+            }
+        }
+    }
+    
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        print("shortcut in")
+        routeShortcut(shortcutItem.type)
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
