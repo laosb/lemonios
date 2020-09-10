b152ecfc1fe903c55239ecc01cb668e878e2b58b
Shibo Lyu <laosb@lns.unihubs.com>
2020-09-10 15:20:47 +0800

    fix: iOS 14 beta 6+ compat.

--- Widgets/ScheduleWidget.swift
+++ Widgets/ScheduleWidget.swift
@@ -10,144 +10,146 @@ import WidgetKit
 import SwiftUI
 
 struct ScheduleProvider: TimelineProvider {
-    public typealias Entry = ScheduleEntry
-    
-    public static let placeholderEntry = ScheduleEntry(date: Date(), items: [
-        LMWidgetScheduleItem(course: "----", classRoom: "-教---", startTime: "--:--", endTime: "--:--", teacher: "--", isTomorrow: false),
-        LMWidgetScheduleItem(course: "----", classRoom: "-教---", startTime: "--:--", endTime: "--:--", teacher: "--", isTomorrow: false),
-    ], errored: false)
+  public typealias Entry = ScheduleEntry
 
-    public func snapshot(with context: Context, completion: @escaping (ScheduleEntry) -> ()) {
-        completion(ScheduleProvider.placeholderEntry)
+  public static let placeholderEntry = ScheduleEntry(date: Date(), items: [
+    LMWidgetScheduleItem(course: "----", classRoom: "-教---", startTime: "--:--", endTime: "--:--", teacher: "--", isTomorrow: false),
+    LMWidgetScheduleItem(course: "----", classRoom: "-教---", startTime: "--:--", endTime: "--:--", teacher: "--", isTomorrow: false),
+  ], errored: false)
+
+  func placeholder(in context: Context) -> ScheduleEntry { Self.placeholderEntry }
+
+  public func getSnapshot(in context: Context, completion: @escaping (ScheduleEntry) -> ()) {
+    completion(ScheduleProvider.placeholderEntry)
+  }
+
+  public func getPlaceholder(in context: Context) -> ScheduleEntry {
+    ScheduleProvider.placeholderEntry
+  }
+
+  public func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    var entries: [ScheduleEntry] = []
+
+    LMWidgetScheduleItem.fetchData { items in
+      guard items != nil else {
+        // Error happened.
+        let timeline = Timeline(entries: [ScheduleEntry(date: Date(), items: [], errored: true)], policy: .atEnd)
+        completion(timeline)
+        return
+      }
+      var remainingItems = items!
+      var startingDate = Date()
+      if remainingItems.isEmpty {
+        entries.append(
+          ScheduleEntry(date: startingDate, items: [], errored: false)
+        )
+        let timeline = Timeline(entries: entries, policy: .after(
+          // Sometimes there could be a schedule change. So refetch after an hour.
+          Calendar.current.date(byAdding: .hour, value: 1, to: Date())!
+        ))
+        completion(timeline)
+        return
+      }
+      while !remainingItems.isEmpty {
+        let firstItem = remainingItems.first!
+        entries.append(
+          ScheduleEntry(date: startingDate, items: remainingItems, errored: false)
+        )
+        startingDate = firstItem.endTimeInDate
+        remainingItems.removeFirst()
+      }
+
+      let timeline = Timeline(entries: entries, policy: .atEnd)
+      completion(timeline)
     }
-
-    public func placeholder(in context: Context) -> ScheduleEntry {
-        ScheduleProvider.placeholderEntry
-    }
-
-    public func timeline(with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [ScheduleEntry] = []
-
-        LMWidgetScheduleItem.fetchData { items in
-            guard items != nil else {
-                // Error happened.
-                let timeline = Timeline(entries: [ScheduleEntry(date: Date(), items: [], errored: true)], policy: .atEnd)
-                completion(timeline)
-                return
-            }
-            var remainingItems = items!
-            var startingDate = Date()
-            if remainingItems.isEmpty {
-                entries.append(
-                    ScheduleEntry(date: startingDate, items: [], errored: false)
-                )
-                let timeline = Timeline(entries: entries, policy: .after(
-                    // Sometimes there could be a schedule change. So refetch after an hour.
-                    Calendar.current.date(byAdding: .hour, value: 1, to: Date())!
-                ))
-                completion(timeline)
-                return
-            }
-            while !remainingItems.isEmpty {
-                let firstItem = remainingItems.first!
-                entries.append(
-                    ScheduleEntry(date: startingDate, items: remainingItems, errored: false)
-                )
-                startingDate = firstItem.endTimeInDate
-                remainingItems.removeFirst()
-            }
-            
-            let timeline = Timeline(entries: entries, policy: .atEnd)
-            completion(timeline)
-        }
-    }
+  }
 }
 
 struct ScheduleEntry: TimelineEntry {
-    let date: Date
-    let items: [LMWidgetScheduleItem]
-    let errored: Bool
+  let date: Date
+  let items: [LMWidgetScheduleItem]
+  let errored: Bool
 }
 
 struct WidgetScheduleEntryView : View {
-    @Environment(\.widgetFamily) var family: WidgetFamily
-    var entry: ScheduleProvider.Entry
-    
-    func itemView (_ item: LMWidgetScheduleItem) -> some View {
-        VStack(alignment: .leading) {
-            Text(item.course).font(.title)
-            Text(item.shortClassRoom).bold() + Text(" · " + item.teacher)
-            Text("\(item.isTomorrow ? "明天" : "")\(item.startTime)-\(item.endTime)")
-        }.frame(minWidth: 0, maxWidth: .infinity)
-    }
-    func emptyView () -> some View {
-        HStack {
-            Spacer()
-            Text("今明两日无课").opacity(0.8)
-            Spacer()
-        }.frame(minHeight: 0, maxHeight: .infinity)
-    }
+  @Environment(\.widgetFamily) var family: WidgetFamily
+  var entry: ScheduleProvider.Entry
 
-    @ViewBuilder
-    var body: some View {
-        VStack {
-            if entry.errored {
-                Spacer()
-                Text("发生错误").opacity(0.8)
-                Spacer()
+  func itemView (_ item: LMWidgetScheduleItem) -> some View {
+    VStack(alignment: .leading) {
+      Text(item.course).font(.title)
+      Text(item.shortClassRoom).bold() + Text(" · " + item.teacher)
+      Text("\(item.isTomorrow ? "明天" : "")\(item.startTime)-\(item.endTime)")
+    }.frame(minWidth: 0, maxWidth: .infinity)
+  }
+  func emptyView () -> some View {
+    HStack {
+      Spacer()
+      Text("今明两日无课").opacity(0.8)
+      Spacer()
+    }.frame(minHeight: 0, maxHeight: .infinity)
+  }
+
+  @ViewBuilder
+  var body: some View {
+    VStack {
+      if entry.errored {
+        Spacer()
+        Text("发生错误").opacity(0.8)
+        Spacer()
+      } else {
+        switch family {
+        case .systemSmall:
+          VStack(alignment: .leading) {
+            Text("下节课").font(.footnote).bold().foregroundColor(.accentColor)
+            Spacer()
+            if entry.items.count > 0 {
+              itemView(entry.items[0])
             } else {
-                switch family {
-                case .systemSmall:
-                    VStack(alignment: .leading) {
-                        Text("下节课").font(.footnote).bold().foregroundColor(.accentColor)
-                        Spacer()
-                        if entry.items.count > 0 {
-                            itemView(entry.items[0])
-                        } else {
-                            emptyView()
-                        }
-                    }
-                case .systemMedium:
-                    VStack(alignment: .leading) {
-                        Text("之后的课").font(.footnote).bold().foregroundColor(.accentColor)
-                        Spacer()
-                        if entry.items.count > 0 {
-                            HStack(alignment: .bottom) {
-                                ForEach(entry.items) { item in
-                                    itemView(item)
-                                }
-                            }.frame(minWidth: 0, maxWidth: .infinity)
-                        } else {
-                            emptyView()
-                        }
-                    }
-                default: Text("尚不支持此尺寸")
-                }
+              emptyView()
             }
-        }.padding()
-    }
+          }
+        case .systemMedium:
+          VStack(alignment: .leading) {
+            Text("之后的课").font(.footnote).bold().foregroundColor(.accentColor)
+            Spacer()
+            if entry.items.count > 0 {
+              HStack(alignment: .bottom) {
+                ForEach(entry.items) { item in
+                  itemView(item)
+                }
+              }.frame(minWidth: 0, maxWidth: .infinity)
+            } else {
+              emptyView()
+            }
+          }
+        default: Text("尚不支持此尺寸")
+        }
+      }
+    }.padding()
+  }
 }
 
 struct ScheduleWidget: Widget {
-    private let kind: String = "help.hdu.lemonios.WidgetSchedule"
+  private let kind: String = "help.hdu.lemonios.WidgetSchedule"
 
-    public var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: ScheduleProvider()) { entry in
-            WidgetScheduleEntryView(entry: entry)
-        }
-        .configurationDisplayName("下节课")
-        .description("显示接下来的几节课。")
-        .supportedFamilies([.systemSmall, .systemMedium])
+  public var body: some WidgetConfiguration {
+    StaticConfiguration(kind: kind, provider: ScheduleProvider()) { entry in
+      WidgetScheduleEntryView(entry: entry)
     }
+    .configurationDisplayName("下节课")
+    .description("显示接下来的几节课。")
+    .supportedFamilies([.systemSmall, .systemMedium])
+  }
 }
 
 struct ScheduleWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            WidgetScheduleEntryView(entry: ScheduleProvider.placeholderEntry)
-                .previewContext(WidgetPreviewContext(family: .systemSmall))
-            WidgetScheduleEntryView(entry: ScheduleProvider.placeholderEntry)
-                .previewContext(WidgetPreviewContext(family: .systemMedium))
-        }
+  static var previews: some View {
+    Group {
+      WidgetScheduleEntryView(entry: ScheduleProvider.placeholderEntry)
+        .previewContext(WidgetPreviewContext(family: .systemSmall))
+      WidgetScheduleEntryView(entry: ScheduleProvider.placeholderEntry)
+        .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
+  }
 }
--- lemonios/SignInWithAppleView.swift
+++ lemonios/SignInWithAppleView.swift
@@ -11,121 +11,121 @@ import SwiftUI
 import Alamofire
 
 struct SignInWithAppleView: UIViewRepresentable {
-    
-    // Success: true, token
-    // Failure: false, reason
-    var onFinish: (Bool, String) -> Void
-    var dismissLogin: () -> Void
-    var alertToBind: (Bool, URL) -> Void
-    
-    class Coordinator: NSObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
-        let parent: SignInWithAppleView?
-        
-        init(_ parent: SignInWithAppleView) {
-            self.parent = parent
-            super.init()
-            
+
+  // Success: true, token
+  // Failure: false, reason
+  var onFinish: (Bool, String) -> Void
+  var dismissLogin: () -> Void
+  var alertToBind: (Bool, URL) -> Void
+
+  class Coordinator: NSObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    let parent: SignInWithAppleView?
+
+    init(_ parent: SignInWithAppleView) {
+      self.parent = parent
+      super.init()
+
+    }
+
+    @objc func didTapButton() {
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.presentationContextProvider = self
+      authorizationController.delegate = self
+      authorizationController.performRequests()
+    }
+
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+      let vc = UIApplication.shared.windows.last?.rootViewController
+      return (vc?.view.window!)!
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+      guard let credentials = authorization.credential as? ASAuthorizationAppleIDCredential else {
+        print("credentials not found....")
+        return
+      }
+
+      AF.request(
+        "https://api.hduhelp.com/login/callback/apple",
+        method: .post,
+        parameters: [
+          "clientID": "app",
+          "school": "hdu",
+          "bind": true,
+          "redirect": "lemonios",
+          "code": String(
+            data: credentials.authorizationCode!,
+            encoding: .ascii
+          )!,
+          "appleClientID": Bundle.main.bundleIdentifier!
+        ],
+        encoding: URLEncoding.queryString,
+        headers: [ "User-Agent": "Lemon_iOS" ]
+      ).validate().responseData{ res in
+        if case .success(let content) = res.result {
+          print("data:", content.base64EncodedString())
         }
-        
-        @objc func didTapButton() {
-            let appleIDProvider = ASAuthorizationAppleIDProvider()
-            let request = appleIDProvider.createRequest()
-            request.requestedScopes = [.fullName, .email]
-            
-            let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-            authorizationController.presentationContextProvider = self
-            authorizationController.delegate = self
-            authorizationController.performRequests()
-        }
-        
-        func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-            let vc = UIApplication.shared.windows.last?.rootViewController
-            return (vc?.view.window!)!
-        }
-        
-        func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-            guard let credentials = authorization.credential as? ASAuthorizationAppleIDCredential else {
-                print("credentials not found....")
-                return
+      }.responseJSON { res in
+        switch res.result {
+        case .success(let val):
+          let value = val as! NSDictionary
+          let data = value.object(forKey: "data") as! NSDictionary
+          let isAuthed = data.object(forKey: "authorize") as? Bool ?? false
+          if isAuthed {
+            let token = data.object(forKey: "accessToken") as! String
+            self.parent?.onFinish(true, token)
+          } else {
+            let needBind = data.object(forKey: "needBind") as? Bool ?? false
+            if needBind {
+
+
+              let urlStr = data.object(forKey: "bindURL") as! String
+              let url = URL(string: urlStr)
+              guard url != nil else { return }
+              //                            UIApplication.shared.open(url!)
+              self.parent?.alertToBind(true, url!)
+              //                            self.parent?.dismissLogin()
+            } else {
+              self.parent?.onFinish(false, "Apple 登录失败，请重试")
             }
-            
-            AF.request(
-                "https://api.hduhelp.com/login/callback/apple",
-                method: .post,
-                parameters: [
-                    "clientID": "app",
-                    "school": "hdu",
-                    "bind": true,
-                    "redirect": "lemonios",
-                    "code": String(
-                        data: credentials.authorizationCode!,
-                        encoding: .ascii
-                    )!,
-                    "appleClientID": Bundle.main.bundleIdentifier!
-                ],
-                encoding: URLEncoding.queryString,
-                headers: [ "User-Agent": "Lemon_iOS" ]
-            ).validate().responseData{ res in
-                if case .success(let content) = res.result {
-                    print("data:", content.base64EncodedString())
-                }
-            }.responseJSON { res in
-                switch res.result {
-                case .success(let val):
-                    let value = val as! NSDictionary
-                    let data = value.object(forKey: "data") as! NSDictionary
-                    let isAuthed = data.object(forKey: "authorize") as? Bool ?? false
-                    if isAuthed {
-                        let token = data.object(forKey: "accessToken") as! String
-                        self.parent?.onFinish(true, token)
-                    } else {
-                        let needBind = data.object(forKey: "needBind") as? Bool ?? false
-                        if needBind {
-                            
-                            
-                            let urlStr = data.object(forKey: "bindURL") as! String
-                            let url = URL(string: urlStr)
-                            guard url != nil else { return }
-//                            UIApplication.shared.open(url!)
-                            self.parent?.alertToBind(true, url!)
-//                            self.parent?.dismissLogin()
-                        } else {
-                            self.parent?.onFinish(false, "Apple 登录失败，请重试")
-                        }
-                    }
-                case .failure(let error):
-                    print("result:", res.result)
-                    self.parent?.onFinish(false, "Apple 登录失败。请重试。")
-                }
-            }
+          }
+        case .failure(let _):
+          print("result:", res.result)
+          self.parent?.onFinish(false, "Apple 登录失败。请重试。")
         }
-        
-        func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        }
+      }
     }
-    
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(self)
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
     }
-    
-    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
-        let button = ASAuthorizationAppleIDButton(
-            authorizationButtonType: .signIn,
-            authorizationButtonStyle: .black
-        )
-        button.frame = .zero
-        button.cornerRadius = 30
-        button.addTarget(
-            context.coordinator,
-            action: #selector(Coordinator.didTapButton),
-            for: .touchUpInside
-        )
-        
-        return button
-    }
-    
-    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
-    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(self)
+  }
+
+  func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+    let button = ASAuthorizationAppleIDButton(
+      authorizationButtonType: .signIn,
+      authorizationButtonStyle: .black
+    )
+    button.frame = .zero
+    button.cornerRadius = 30
+    button.addTarget(
+      context.coordinator,
+      action: #selector(Coordinator.didTapButton),
+      for: .touchUpInside
+    )
+
+    return button
+  }
+
+  func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
+  }
 }
 
 //struct SignInWithAppleView_Previews: PreviewProvider {
